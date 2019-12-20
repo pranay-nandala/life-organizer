401297b8be7a385f02f6384caeb05ded130117ac
pranay nandala <pranaynse@outlook.com>
2019-12-20 08:19:07 +0530

    new production build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -6,14 +6,14 @@ const precacheFiles = [
 ];
 
 self.addEventListener("install", function (event) {
-  console.log("[PWA Builder] Install Event processing");
+  console.log("Install Event processing");
 
-  console.log("[PWA Builder] Skip waiting on install");
+  console.log("Skip waiting on install");
   self.skipWaiting();
 
   event.waitUntil(
     caches.open(CACHE).then(function (cache) {
-      console.log("[PWA Builder] Caching pages during install");
+      console.log("Caching pages during install");
       return cache.addAll(precacheFiles);
     })
   );
@@ -21,7 +21,7 @@ self.addEventListener("install", function (event) {
 
 // Allow sw to control of current page
 self.addEventListener("activate", function (event) {
-  console.log("[PWA Builder] Claiming clients for current page");
+  console.log("Claiming clients for current page");
   event.waitUntil(self.clients.claim());
 });
 
@@ -54,7 +54,7 @@ self.addEventListener("fetch", function (event) {
             return response;
           })
           .catch(function (error) {
-            console.log("[PWA Builder] Network request failed and no cache." + error);
+            console.log("Network request failed and no cache." + error);
           });
       }
     )
